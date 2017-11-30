4855829f5c851644f4cceab04f0b5c90a9b081f3
tetra <33928988+tetrahidrocanab@users.noreply.github.com>
2017-11-30 10:30:02 +0100

    Update billetes.c

--- billetes.c
+++ billetes.c
@@ -25,16 +25,16 @@ int CalcularBilletes(void){
     int  billetes50, billetes20, billetes10, billetes5, monedas2, monedas1, sueldo50, sueldo20, sueldo10, sueldo5, sueldo2, sueldo1;
     printf("Devolucion de nuestro sueldo en billetes y monedas \n");
     
-    //Leo el sueldo que tengo 
+     //A...calculo el sueldo
     printf("Sueldo: \n");
     scanf("%i",&sueldo);  
-      //Operacion para calcular los billetes de 500
+      //A...operaciones para fraccionar el sueldo en billetes o monedas
     operacion500 = sueldo / 500;
     sueldo500 = operacion500;
-     //Operacion para calcular los billetes de 200
+     
     operacion200 = sueldo - (operacion500 * 500);
     sueldo200 = operacion200/200;
-       //Operacion para calcular los billetes de 100
+       
     operacion100 = sueldo - (operacion200 * 200);
     sueldo100 = operacion100/100;
     
@@ -56,7 +56,7 @@ int CalcularBilletes(void){
     monedas1 = sueldo - (monedas2 * 2);
     sueldo1 = monedas1;
             
-        
+        //A...si el sueldo es inferior o igual a 6000 se imprime el resultado(de 500 a 50 euros)
     if (sueldo <= 6000){
         printf("El sueldo en billetes de 500 es: %i \n", sueldo500); 
         printf("El sueldo en billetes de 200 es: %i \n", sueldo200);
@@ -64,14 +64,14 @@ int CalcularBilletes(void){
         printf("El sueldo en billetes de 50 es: %i \n", sueldo50);
         
     }
-    else if(sueldo <50){
+    else if(sueldo <50){ //A...si el sueldo es inferior a 50 se imprimen la fraccion del sueldo en billetes y monedas de 50 a 1 euro
                 printf("El sueldo en billetes de 20 es: %i \n", sueldo20);
                 printf("El sueldo en billetes de 10 es: %i \n", sueldo10);
                 printf("El sueldo en billetes de 5 es: %i \n", sueldo5);
                 printf("El sueldo en monedas de 2 es: %i \n", sueldo2);
                 printf("El sueldo en monedas de 1 es: %i \n", sueldo1);
     }
-    else{
+    else{ //A...si no es ninguno de los casos anteriores no es valido
         printf("El sueldo no entra dentro de los valores establecidos \n");
     } 
     
