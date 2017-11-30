87ad19a2bb81bdb9eeb4e5e092351b6722abfa8c
Programación <Programacin@PC07>
2017-11-30 12:09:38 +0100

    Update main.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int validar();
+//int CalcularBilletes();
 
 int main() {
     
@@ -20,25 +21,25 @@ int main() {
     printf("\t3.- Calcular billetes.\n");
     
     scanf("%s",&eleccion);
-    rep = 0; //variable para repetir el bucle.
+    rep = 0;
         switch (eleccion) {
             case '0':
                 printf("\nHas elegido finalizar.");
                 break;
             case '1':
-                modo = 2; //variable para determinar que modo de validacion se usa.
+                modo = 3;
                 printf("\nHas elegido calcular IMC.\n");
                 printf("\nEscribe tu peso(kg).\n\n");
                 scanf("%s", &peso);
                 validar(peso, modo);
+                modo = 1;
                 printf("\nEscribe tu altura(cm).\n\n");
                 scanf("%s",&altura);
-                modo = 1;
                 validar(altura, modo);
                 rep = 1;
                 break;
             case '2':
-                modo = 1;
+                modo = 2;
                 printf("\nHas elegido calcular romanos.\n\n");
                 printf ("Introduce un número entre 1 y 3500.\n\n");
                 scanf ("%s", &numero);
@@ -46,17 +47,17 @@ int main() {
                 rep = 1;
                 break;
             case '3':
-                modo = 2;
+                modo = 3;
                 printf("\nHas elegido calcular los billetes.\n\n");
                 printf("Introduce tu sueldo.\n\n");
                 scanf("%s",&sueldo);
                 validar(sueldo, modo);
                 rep = 1;
                 break;
-            default:    //opcion que usa si no es ninguna de las anteriores.
+            default:    //opcion que usa si no es ninguna de las anteriores
                 printf("\nOpción no válida.\n");
                 rep = 1;
                 break;
             }
-    }while(rep!=0); //repite el bucle mientras rep sea distinto de 0.
+    }while(rep!=0);
 }
