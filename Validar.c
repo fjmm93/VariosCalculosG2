489fb8ebe3a2103c7ea89b6acf26b8366b56df72
fjmm93 <33929004+fjmm93@users.noreply.github.com>
2017-11-30 12:46:02 +0100

    Update Validar.c

--- Validar.c
+++ Validar.c
@@ -8,18 +8,19 @@ int validar (char *numero, int modo)
     int comp;   //variable que determina si algun caracter es digital. 
     largo = strlen(numero);  //mide la longitud de la cadena.
     comp=0;
+    int stop; //funcion que para la ejecucion si hay alguna letra.
     
 
     // Recorremos la cadena (el dato introducido) para comprobar su tipo.
     if (modo <=2)
         {
-        for (int i=0; i<largo; ++i){   //mientras i sea menor que largo i se ira incrementando de 1 en 1.
+        for (int i=0; i<largo && comp == 0; ++i){   //mientras 'i' sea menor que largo y comp sea 0 'i' se ira incrementando de 1 en 1.
             if (!isdigit (numero[i])) comp++;   // si hay alguna letra comp se incrementa.
         }
     
         if (comp!=0)
-        { printf ("\nNo válido, introduce sólo números.\n");
-    
+        { 
+            printf ("\nNo válido, introduce sólo números.\n");
         }    
 
         }
