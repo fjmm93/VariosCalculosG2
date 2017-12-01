c819480a537ee9b4295bbbd8a726531ccec40203
Programación <Programacin@PC07>
2017-12-01 13:37:00 +0100

    Update Validar, añadir comentarios y correccion de errores.

--- Validar.c
+++ Validar.c
@@ -8,29 +8,30 @@ int validar (char *numero, int modo)
     int comp;   //variable que determina si algun caracter es digital. 
     largo = strlen(numero);  //mide la longitud de la cadena.
     comp=0;
-    int stop = 0;    
-    
-    for (int i=0; i<largo; ++i){   //mientras 'i' sea menor que largo y comp sea 0 'i' se ira incrementando de 1 en 1.
-            if (!isdigit (numero[i])) comp++;   // si hay alguna letra comp se incrementa.
-        }
+    int stop = 0;    //variable que permite volver a pedir el numero (en main) mientras haya alguna letra (con 0 no repite con 1 si)
     
     // Recorremos la cadena (el dato introducido) para comprobar su tipo.
+    for (int i=0; i<largo; ++i){   //mientras 'i' sea menor que largo y comp sea 0 'i' se ira incrementando de 1 en 1.
+            if (!isdigit (numero[i])) comp++;   // si hay alguna letra comp se incrementa.
+    }
     
-    if (modo <=2 && comp != 0){        
+    if (modo <=2 && comp != 0){        //si modo es menor o igual a 2 y comp es distinto de 0 da error.
             printf ("\nNo válido, introduce sólo números.\n");
             stop = 1;
     }          
-    if (comp == 1){ //si solo hay un caracter no numérico.
-        for (int i=0; i<largo && comp == 0; ++i){   //mientras 'i' sea menor que largo y comp sea 0 'i' se ira incrementando de 1 en 1.
-        if (numero[i] == '.'){   //comprueba si ese caracter es la coma. 
-        comp = 0;       //si es la coma comp vuelve a 0 y no da fallo.
-        }
-        }
-    }
     else {
-        printf ("\nNo válido, introduce sólo números.\n\n");
-        stop = 1;
-    }
+        
+        if (comp < 2){ //si solo hay un caracter no numérico.
+        for (int i=0; i<largo && comp == 0; ++i){   //mientras 'i' sea menor que largo y comp sea 0 'i' se ira incrementando de 1 en 1.
+            if (numero[i] == '.'){   //comprueba si ese caracter es la coma. 
+            comp = 0;       //si es la coma comp vuelve a 0 y no da fallo.
+            }
+        }
+        }
+        else {
+            printf ("\nNo válido, introduce sólo números.\n\n");
+            stop = 1;
+        }
     return stop;
+    }
 }
-
