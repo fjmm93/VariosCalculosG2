3be8a6f2bef5054e616bd27767ddddfa6dc7f9a1
Programación <Programacin@PC07>
2017-12-01 13:27:40 +0100

    Update MAIN, añadir comentarios y llamada a funciones.

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 
 int validar();
-//int CalcularBilletes();
+int CalcularBilletes();
+int MaxBilletes();
+int romanos();
+int Imc();
 
 int main() {
     
@@ -11,7 +14,7 @@ int main() {
     char peso[20];
     char altura[20];
     char sueldo[20];
-    int rep, modo;
+    int rep, modo, prueba;
     
     do{
     printf("\nElige una opción:\n");
@@ -27,34 +30,48 @@ int main() {
                 printf("\nHas elegido finalizar.");
                 break;
             case '1':
-                modo = 3; //A...cuando coge 3 el programa acepta la coma. variable que indica que tipo de validacion se realiza.
                 printf("\nHas elegido calcular IMC.\n");
+                do{
+                modo = 3;//A...cuando coge 3 el programa acepta la coma. variable que indica que tipo de validacion se realiza.
                 printf("\nEscribe tu peso(kg).\n\n");
                 scanf("%s", &peso);
-                validar(peso, modo);    //se llama a la funcion validar y se le da los valores peso y modo.
+                prueba = validar(peso, modo);//se llama a la funcion validar y se le da los valores peso y modo.
+                }while(prueba != 0);    //si la funcion validar devuelve un valor distinto de 0 repite el proceso.
+                                        //la funcion validar solo devuelve 0 cuando no hay ninguna letra.
+                do{ 
                 modo = 1;//A.. al escoger 1 o 2 (en el modo) la coma no la leeria
                 printf("\nEscribe tu altura(cm).\n\n");
                 scanf("%s",&altura);
-                validar(altura, modo);
+                prueba = validar(altura, modo);
+                }while(prueba != 0);
+                Imc(peso, altura);
                 rep = 1;
                 break;
             case '2':
-                modo = 2;
                 printf("\nHas elegido calcular romanos.\n\n");
+                do{
+                modo = 2;
                 printf ("Introduce un número entre 1 y 3500.\n\n");
                 scanf ("%s", &numero);
-                validar(numero, modo);
+                prueba = validar(numero, modo);
+                }while(prueba != 0);
+                int num;
+                num = atoi(numero);
+                romanos(num);
                 rep = 1;
                 break;
             case '3':
-                modo = 3;
                 printf("\nHas elegido calcular los billetes.\n\n");
+                do{
+                modo = 3;
                 printf("Introduce tu sueldo.\n\n");
                 scanf("%s",&sueldo);
-                validar(sueldo, modo);
+                prueba = validar(sueldo, modo);
+                }while(prueba != 0);
+                MaxBilletes(sueldo);
                 rep = 1;
                 break;
-            default:    //opcion que usa si no es ninguna de las anteriores.
+            default:    //opcion que usa si no es ninguna de las anteriores
                 printf("\nOpción no válida.\n");
                 rep = 1;
                 break;
